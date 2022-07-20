8fb2b373acb152ee73e160770996a2137c9d9b4d
xT10r <J03eF@yahoo.com>
2022-07-20 12:27:49 +0300

    HW5
    Add files

---
+++ DevSecOps/05-HW/example-01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Фрагмент кода 1:
+
+from flask import Flask
+from mod_api import mod_api
+
+app = Flask('vuln_app')
+app.config['SECRET_KEY'] = 'F0cUzh8BgYJSLXAU8qDmClM0dE8GJTpsiyVEl3BCqQMCABp1U$f%'
+app.register_blueprint(mod_api, url_prefix='/api')
---
+++ DevSecOps/05-HW/example-03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Фрагмент кода 3:
+
+def post(self):
+    username = self.get_argument('username')
+    password = self.get_argument('password').encode('utf-8')
+    email = self.get_argument('mail')
+    try:
+        username = username.lower()
+        email = email.strip().lower()
+        user = User({'username': username, 'password': password, 'email': email, 'date_joined': curtime()})
+        user.validate()
+        save_user(self.db_conn, user)
+
+    except Exception, e:
+
+        return self.render_template("success_create.html")
